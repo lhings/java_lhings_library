4ac9a7fd9cd66747e9c7efa20ea3a0891c6d1ca8
Jose Antonio Lorenzo <jose@lhings.com>
2016-11-02 17:14:32 +0100

    Added comments in logback.groovy. Changed version string to match 2.4.3.

--- logback.groovy
+++ logback.groovy
@@ -1,19 +1,26 @@
 // Logging configuration
+// Default log level
 def ROOT_LOGGER_LOG_LEVEL = DEBUG
+
+// Definition of loggers and their corresponding log level
 def loggers = [
 		'com.lhings.java': DEBUG,
 	]
-
+// Maximum total size of all log files
 def MAX_LOG_SIZE = "20MB"
+// Maximum number of days logs will be stored
 def MAX_LOG_HISTORY = 10
+// Name of the default log file
 def LOG_FILENAME = "logback"
 
+// Pattern used for logging
 def PATTERN = "%d{HH-mm-ss.SSS} [%10.10thread] %5level in %-30logger{30} - %msg%n"
 
 
 
 
-
+// Logging definition. 
+// WARNING: do not change anything beyond this line, unless you know what you're doing.
 
 import ch.qos.logback.core.util.FileSize
 
